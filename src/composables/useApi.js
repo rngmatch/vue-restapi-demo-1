58da9be5250b95460372f4fe8bf3dd9ef249c2ed
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-02-28 05:40:08 -0600

    removed changes

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,24 +1,11 @@
 import axios from 'axios'
-import { ref } from 'vue'
 
 const instance = axios.create({
   baseURL: 'https://api.disneyapi.dev/',
 })
 
-const page = ref(1)
-
 const useApi = () => {
-  const getCharaters = async () => {
-    const { data } = await instance.get('/charaters', {
-      params: {
-        page: page.value,
-      },
-    })
-    page.value++
-    return data
-  }
-
-  return { getCharaters }
+  return { instance }
 }
 
 export default useApi
