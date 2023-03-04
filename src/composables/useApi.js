36f57021833e65423196a7170e9d20a14256ee2e
Martin Rodriguez <mrodriguez9987@yahoo.com>
2023-03-04 12:26:26 -0600

    Starting

--- src/composables/useApi.js
+++ src/composables/useApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const instance = axios.create({
-  baseURL: 'https://api.disneyapi.dev/',
+const instance = axios.get({
+  baseURL: 'https://mhw-db.com/monsters/',
 })
 
 const useApi = () => {
